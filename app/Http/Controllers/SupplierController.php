b17e9c33daa6c69a14a54ac23bcddf9eeb49fe98
grox0d <yp14yj@gmail.com>
2026-02-06 22:34:01 +0700

    `Updated stock and supplier views with new features and bug fixes`

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class SupplierController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('supplier', [
-            'suppliers' => Supplier::latest()->get()
-        ]);
+        $search = $request->input('search', '');
+        
+        $query = Supplier::oldest();
+        
+        if ($search) {
+            $query->where('nama', 'like', '%' . $search . '%')
+                  ->orWhere('alamat', 'like', '%' . $search . '%')
+                  ->orWhere('telepon', 'like', '%' . $search . '%');
+        }
+        
+        $suppliers = $query->get();
+        return view('supplier', compact('suppliers', 'search'));
     }
 
     public function store(Request $request)
@@ -44,5 +54,29 @@ class SupplierController extends Controller
         $supplier->delete();
 
         return back()->with('success', 'Supplier dihapus');
+    }
+
+    public function exportPdf(Request $request)
+    {
+        $search = $request->input('search', '');
+        
+        $query = Supplier::oldest();
+        
+        if ($search) {
+            $query->where('nama', 'like', '%' . $search . '%')
+                  ->orWhere('alamat', 'like', '%' . $search . '%')
+                  ->orWhere('telepon', 'like', '%' . $search . '%');
+        }
+        
+        $suppliers = $query->get();
+        
+        $pdf = Pdf::loadView('pdf.supplier', ['suppliers' => $suppliers])
+                  ->setPaper('a4')
+                  ->setOption('margin-top', 10)
+                  ->setOption('margin-bottom', 10)
+                  ->setOption('margin-left', 10)
+                  ->setOption('margin-right', 10);
+        
+        return $pdf->download('daftar-supplier-' . date('Y-m-d') . '.pdf');
     }
 }
